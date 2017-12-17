14c0b48b22cc315c7bbe9a3499b37ffd7f0e36d7
M977986310 <977986310@qq.com>
2017-12-17 18:40:40 +0800

    完成文件上传,解压以及相关的文件信息存入到数据库

--- src/main/java/com/thoughtWorks/dao/UploadDao.java
+++ src/main/java/com/thoughtWorks/dao/UploadDao.java
@@ -6,4 +6,14 @@ import java.util.Map;
 
 public interface UploadDao {
     void addZipInfo(@Param("zipFile") Map<String, Object> zipFile);
+
+    Map<String,Object> getModelGrade(@Param("data") Map<String, Object> data);
+
+    String getModelGradeOne(@Param("count0") Object count0);
+
+    String getModelGradeTwo(@Param("count1") Object count1);
+
+    String getModelGradeThr(@Param("count2") Object count2);
+
+    void addModelGrade(@Param("modelGrade") Map<String, Object> modelGrade);
 }
--- src/main/java/com/thoughtWorks/service/impl/UploadServiceImpl.java
+++ src/main/java/com/thoughtWorks/service/impl/UploadServiceImpl.java
@@ -17,21 +17,42 @@ public class UploadServiceImpl implements UploadService {
 
     @Override
     public void addZipInfo(Map<String, Object> zipFile) {
-//        System.out.println("serviceImpl:" + zipFile);
         String code = (String) zipFile.get("code");
         uploadDao.addZipInfo(zipFile);
         //提取关于类型的信息
-        System.out.println(extractFileInformation(zipFile));
+        Map<String, Object> data = extractFileInformation(zipFile);
 
-//        uploadDao.getModelGrade(zipFile);
+        uploadDao.addModelGrade(getModelGrade(code, data));
+
+    }
+
+    private Map<String,Object> getModelGrade(String code, Map<String, Object> data) {
+        Map<String, Object> modelGradeInfo = new HashMap<>();
+        String oneId = null;
+        String twoId = null;
+        String thrId = null;
+        modelGradeInfo.put("code", code);
+        if (data.get("count0") != "") {
+            oneId = uploadDao.getModelGradeOne(data.get("count0"));
+            modelGradeInfo.put("oneId", oneId);
+        }
+        if (data.get("count1") != "") {
+            twoId = uploadDao.getModelGradeTwo(data.get("count1"));
+            modelGradeInfo.put("twoId", twoId);
+        }
+        if (data.get("count2") != "") {
+            thrId = uploadDao.getModelGradeThr(data.get("count2"));
+            modelGradeInfo.put("thrId", thrId);
+        }
+
+        return modelGradeInfo;
     }
 
     private Map<String, Object> extractFileInformation(Map<String, Object> zipFile) {
         Map<String, Object> modelGrade = new HashMap<>();
 
         String[] types = ((String) zipFile.get("type")).split("\\#");
-        System.out.println("types" + Arrays.toString(types));
-        if (types.length > 3) {
+        if (types.length == 3) {
             for (int i = 0; i < 3; i++) {
                 modelGrade.put("count" + i, types[i]);
             }
