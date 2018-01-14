2788637d095eb1b4c3361fc8f198ef1c92d51f75
M977986310 <977986310@qq.com>
2018-01-14 20:47:15 +0800

    修复相同id产品上传问题

--- src/main/java/com/thoughtWorks/service/impl/UploadServiceImpl.java
+++ src/main/java/com/thoughtWorks/service/impl/UploadServiceImpl.java
@@ -18,14 +18,18 @@ public class UploadServiceImpl implements UploadService {
     @Override
     public void addZipInfo(Map<String, Object> zipFile) {
         String code = (String) zipFile.get("code");
+        int resultCount = uploadDao.checkCode(code);
+
         //提取关于类型的信息
         Map<String, Object> data = extractFileInformation(zipFile);
 
-
         Map<String,Object> zipData = getModelGrade(code, zipFile,data);
 
-
-        uploadDao.addZipInfo(zipData);
+        if (resultCount == 0) {
+            uploadDao.addZipInfo(zipData);
+        }else {
+            uploadDao.upZipInfo(zipData);
+        }
     }
 
     private Map<String, Object> getModelGrade(String code, Map<String, Object> zipFiles, Map<String, Object> data) {
